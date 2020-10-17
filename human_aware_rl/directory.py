00ef527dd0ea340e10c6457f101af810863ddc9d
Kennard Ng <kennard_ng@outlook.com>
2020-10-18 01:16:18 +0800

    fix pathing

--- human_aware_rl/directory.py
+++ human_aware_rl/directory.py
@@ -1,10 +1,16 @@
 import platform
+import getpass
 import os
+
 
 node = platform.node()
 if node == 'kennardng-desktop':
     PROJECT_DIR = '/home/kennardng/Desktop/cs6244-project'
 else:
-    raise NotImplementedError
+    user_id = getpass.getuser()
+    if user_id == 'e0036319':
+        PROJECT_DIR = '/home/e/e0036319/code/cs6244-project'
+    else:
+        raise NotImplementedError
 DATA_DIR = os.path.join(PROJECT_DIR, 'human_aware_rl', 'data')
 CHECKPOINT_DIR = os.path.join(PROJECT_DIR, 'checkpoints')
--- readme.md
+++
@@ -1,9 +0,0 @@
-
-## Setting Up
-
-```
-conda create -n cs6244-project python==3.6.8
-conda activate cs6244-project
-pip install -r requirements.txt --use-feature=2020-resolver
-pip install -e overcooked_ai/
-```
